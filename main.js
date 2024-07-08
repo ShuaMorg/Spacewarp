8c5bbbe50b93cdcda09ac74a7daa7a49604642c7
ShuaMorg <73347450+ShuaMorg@users.noreply.github.com>
2024-07-08 22:47:13 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,17 +2,4 @@ document.getElementById('startButton').addEventListener('click', () => {
   goFullScreen();
   document.getElementById('startButton').style.display = 'none';
   init();
-  displayMessages(); // Start displaying messages
 });
-
-function goFullScreen() {
-  if (document.documentElement.requestFullscreen) {
-    document.documentElement.requestFullscreen();
-  } else if (document.documentElement.mozRequestFullScreen) {
-    document.documentElement.mozRequestFullScreen();
-  } else if (document.documentElement.webkitRequestFullscreen) {
-    document.documentElement.webkitRequestFullscreen();
-  } else if (document.documentElement.msRequestFullscreen) {
-    document.documentElement.msRequestFullscreen();
-  }
-}
