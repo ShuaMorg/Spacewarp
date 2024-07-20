71b788aa0edb45a8a682125def4746ac7b8c7ae4
ShuaMorg <73347450+ShuaMorg@users.noreply.github.com>
2024-07-20 15:08:58 +0100

    Update audio_manager.js

--- audio_manager.js
+++ audio_manager.js
@@ -36,8 +36,8 @@ class AudioPoint {
 
 // Create instances of AudioPoint outside the function
 const points = [
-  new AudioPoint(10, 100, -300, 'test.wav'),
-  new AudioPoint(-10, 120, -200, 'test2.wav')
+  new AudioPoint(10, 50, -300, 'test.wav'),
+  new AudioPoint(-10, 20, -200, 'test2.wav')
 ];
 
 
