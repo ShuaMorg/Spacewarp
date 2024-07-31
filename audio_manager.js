0124b18e64e85ff488a9e9a3f7cf122ff21b8275
ShuaMorg <73347450+ShuaMorg@users.noreply.github.com>
2024-07-31 21:45:56 +0100

    Update audio_manager.js

--- audio_manager.js
+++ audio_manager.js
@@ -11,22 +11,27 @@ class AudioPoint {
 
   playAudio() {
     if (!this.isPlaying) {
+      this.audio.loop = true; // Ensure the audio loops continuously
       this.audio.play().catch(error => {
         console.log('Failed to play audio:', error);
       });
       this.isPlaying = true;
-      // Ensure the isPlaying flag is reset when the audio ends
-      this.audio.onended = () => {
-        this.isPlaying = false;
-      };
+    }
+  }
+
+  stopAudio() {
+    if (this.isPlaying) {
+      this.audio.pause();
+      this.audio.currentTime = 0;
+      this.isPlaying = false;
     }
   }
 
   adjustVolume(distance) {
     let volume = 0;
-    if (distance < this.innerRange) {
+    if (distance <= this.innerRange) {
       volume = 1;
-    } else if (distance < this.outerRange) {
+    } else if (distance <= this.outerRange) {
       volume = 1 - (distance - this.innerRange) / (this.outerRange - this.innerRange);
     }
     this.audio.volume = volume;
@@ -39,10 +44,12 @@ class AudioPoint {
       Math.pow(position.z - this.z, 2)
     );
 
-    this.adjustVolume(distance);
-
-    if (distance < this.outerRange) {
+    if (distance <= this.outerRange) {
       this.playAudio();
+      this.adjustVolume(distance);
+    } else {
+      this.adjustVolume(distance);
+      this.stopAudio();
     }
   }
 }
@@ -50,7 +57,7 @@ class AudioPoint {
 // Create instances of AudioPoint with inner and outer ranges
 const points = [
   new AudioPoint(100, 1900, -9000, 'Scene2.m4a', 500, 1000),
-  new AudioPoint(0, 0, -500, 'Scene1.m4a', 150, 350)
+  new AudioPoint(0, 0, -850, 'Scene1.m4a', 300, 800)
 ];
 
 function checkProximityAndPlaySound(position) {
@@ -58,3 +65,5 @@ function checkProximityAndPlaySound(position) {
     point.checkProximity(position);
   });
 }
+
+export { checkProximityAndPlaySound }; // Ensure the function is available for import
