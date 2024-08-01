d8e8e8668ad6ddb71246d0dd13c1472ddc17b065
ShuaMorg <73347450+ShuaMorg@users.noreply.github.com>
2024-08-01 18:46:41 +0100

    Update world.js

--- world.js
+++ world.js
@@ -5,28 +5,25 @@ let worlds = [];
 function createWorlds(scene) {
   const textureLoader = new THREE.TextureLoader();
   
-  // Predetermined random coordinates for worlds
+  // Predetermined random coordinates, textures, and sizes for worlds
   const worldData = [
-    { x: 500, y: 0, z: -1650, texture: 'planet.jpg' },
-
-    { x: -500, y: 0, z: -1650, texture: 'planet.jpg' },
-
-    { x: -50, y: 200, z: -950, texture: 'p7.png' },
-    { x: -50, y: 200, z: -250, texture: 'brown.jpg' },
-
-    // Scene 3
-    
-    { x: 100, y: 1900, z: -25500, texture: 'brown.jpg' }
-
+    { x: -50, y: -50, z: -850, texture: 'planet.jpg', size: 40 },
+    { x: -100, y: 0, z: -450, texture: 'planet.jpg', size: 40},
+    { x: 50, y: 100, z: -650, texture: 'p7.png', size: 40 },
     
     
-    // Add more coordinates and corresponding textures as needed
+    { x: -50, y: 200, z: -25250, texture: 'sun.jpg', size: 1500 },
+ //   { x: -50, y: 200, z: -25250, texture: 'planet.jpg', size: 1000 },
+
+    
+    { x: 100, y: 1900, z: -25500, texture: 'brown.jpg', size: 60 }
+    // Add more coordinates, textures, and sizes as needed
   ];
 
   for (let i = 0; i < worldData.length; i++) {
     const worldTexture = textureLoader.load(worldData[i].texture);  // Load the specific texture for this world
     const worldMaterial = new THREE.MeshPhongMaterial({ map: worldTexture }); // Use MeshPhongMaterial to enable shading
-    const worldGeometry = new THREE.SphereGeometry(30, 32, 32);  // Increased size of worlds
+    const worldGeometry = new THREE.SphereGeometry(worldData[i].size, 32, 32);  // Use specific size for each world
     const world = new THREE.Mesh(worldGeometry, worldMaterial);
     world.position.set(worldData[i].x, worldData[i].y, worldData[i].z);
     scene.add(world);
