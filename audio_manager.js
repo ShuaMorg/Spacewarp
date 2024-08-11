87e9479781c9bc4893ea7ff9dcc61de3f7728cc1
ShuaMorg <73347450+ShuaMorg@users.noreply.github.com>
2024-08-11 15:52:48 +0100

    Add files via upload

--- audio_manager.js
+++ audio_manager.js
@@ -53,7 +53,8 @@ const points = [
 
 // Scene 1
 new AudioPoint(0, 0, 0, 'scene1.m4a', 1220, true),
-  new AudioPoint(-200, 0, -400, 'Vb3.mp3', 280),
+new AudioPoint(15, 0, -950, 'drum.WAV', 700),
+new AudioPoint(-200, 0, -400, 'Vb3.mp3', 280),
 
 
   new AudioPoint(326, -428, -3286, 'Vb3.mp3', 500), // Will play with distance-based volume
