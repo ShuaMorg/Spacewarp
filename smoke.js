5368384012a0a51396af61b7ee4cb2c7656a4ca3
ShuaMorg <73347450+ShuaMorg@users.noreply.github.com>
2024-08-23 16:30:11 +0100

    Update smoke.js

--- smoke.js
+++ smoke.js
@@ -17,7 +17,7 @@ const smokeCoordinates = [
   // Portal is at x: 0, y: 1465, z: 99000
 
   {
-    startX: 0, startY: -100000, startZ: 101500, // Start coordinates
+    startX: 0, startY: -100000, startZ: 101100, // Start coordinates
     middleX: -700, middleY: -99600, middleZ: 100000, // Middle coordinates
     endX: 0, endY: -100000, endZ: 99710, // End coordinates
     texture: 'smoke.png',
